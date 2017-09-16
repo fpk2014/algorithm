d7e135b15231faf40c6cd7df5726c9e4dea3ba07
root <994347033@qq.com>
2017-09-16 21:13:02 +0800

    插入算法独立为函数

--- Sort/InsertSort.cpp
+++ Sort/InsertSort.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 
 using std::cout;
+using std::endl;
+
+void print(int *a, const int length){
+	for(int i=0; i<length; i++){
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+void InsertSort(int *a, const int length){
+	for(int i=1; i<length; i++){
+		int tmp = a[i];
+		int j = i-1;
+		while(j>0 && a[j] > tmp){
+			a[j+1] = a[j];
+			j--;
+		}
+		a[j+1] = tmp;
+		print(a, length);
+	}
+}
+
 
 int main(){
 	const int length = 5;
-	int my_array[length] = {1,5,4,3,7};
-	
-	for(int i=1; i<length; i++){
-		int tmp = my_array[i];
-		int j = i -1;
-		while(j>=0 && my_array[j] > tmp){
-			my_array[j+1] = my_array[j];
-			j--;
-		}
-		my_array[j+1] = tmp;
-	}
+	int my_array[length] = {1,5,4,3,7};	
+	print(my_array, length);
+
+	InsertSort(my_array, length);
 
 
-	for(int i=0; i<length; i++){
-		cout << my_array[i] << " ";
-	}
-	cout << "\n";
 	return 0;
 }
